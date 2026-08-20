ab3828807f29fda118e4f1701fe9d4788939dd43
OlehTkachov <zervannagy@gmail.com>
2026-08-20 12:48:06 +0300

    Wire database status into application

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 from ui.main_window import MainWindow
+from ui.database_status import install_database_status
 
 
 def main():
     app = QApplication(sys.argv)
 
     window = MainWindow()
+    install_database_status(window)
     window.show()
 
     sys.exit(app.exec())
