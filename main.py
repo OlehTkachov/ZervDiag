863eb735042533704feb817dc0017f035a071324
OlehTkachov <zervannagy@gmail.com>
2026-08-21 09:07:42 +0300

    Wire automatic indexing settings into V14

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from ui.main_window import MainWindow
 from ui.database_status import install_database_status
 from ui.table_resizing import install_manual_table_resizing
 from ui.ui_enhancements import install_ui_enhancements
+from ui.auto_indexing import install_auto_index_settings
 
 
 def main():
@@ -15,6 +16,7 @@ def main():
     install_database_status(window)
     install_manual_table_resizing(window)
     install_ui_enhancements(window)
+    install_auto_index_settings(window)
 
     # Запускаем развёрнутым окном: весь рабочий экран,
     # но с обычной рамкой Windows и панелью задач.
