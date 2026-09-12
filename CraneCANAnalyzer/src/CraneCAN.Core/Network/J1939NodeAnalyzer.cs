98cbafaf1bf477ae276be1a7abe282db5355d777
OlehTkachov <zervannagy@gmail.com>
2026-09-12 13:10:47 +0300

    Prepare J1939 node aggregation

--- CraneCANAnalyzer/src/CraneCAN.Core/Network/J1939NodeAnalyzer.cs
+++ CraneCANAnalyzer/src/CraneCAN.Core/Network/J1939NodeAnalyzer.cs
@@ -1,5 +1,11 @@
+using CraneCAN.Core.Models;
+
 namespace CraneCAN.Core.Network;
 
 internal static class J1939NodeAnalyzer
 {
+    public static IReadOnlyList<NetworkNodeSnapshot> Build(IReadOnlyList<CanFrame> frames)
+    {
+        return Array.Empty<NetworkNodeSnapshot>();
+    }
 }
